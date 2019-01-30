f12d1ab173aea63028dc359609b8c2ac580a1e70
iumentum666 <45870466+iumentum666@users.noreply.github.com>
2019-01-30 15:11:35 +0100

    avslutter

---
+++ Kvotesystemet/Models/SpKvoter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kvotesystemet.Models
+{
+    public partial class SpKvoter
+    {
+        public double? Ansattnr { get; set; }
+        public string T0E { get; set; }
+        public string T0ENavn { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public double? TotaltØl { get; set; }
+        public double? TotaltBrus { get; set; }
+        public double? TotaltGratis { get; set; }
+    }
+}
